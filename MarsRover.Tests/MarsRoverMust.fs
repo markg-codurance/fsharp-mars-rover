e7a18b7a2bd3e3cf360ff30e92a4414a184744ee
Mark Gray <mark.gray@codurance.com>
2019-11-26 14:18:59 +0000

    Git initial commit

---
+++ MarsRover.Tests/MarsRoverMust.fs
@@ -0,0 +1,71 @@
+module MarsRoverMust
+
+open NUnit.Framework
+open MarsRover
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let ``initialise at 0,0 facing north`` () =
+    let rover = initialiseRover()
+    let expectedState = { X=0;Y=0;Facing=North }
+    let roverState = navigator { return rover }
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face east after using the navigation system to turn right`` () =
+    let expectedState = { X=0;Y=0;Facing=East }
+    let roverState = navigator { turnRight }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face south after using the navigation system to turn right while facing east initially`` () =
+    let expectedState = { X=0;Y=0;Facing=South }
+    let roverState = navigator { turnRight; turnRight }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face west after using the navigation system to turn right after facing south`` () =
+    let expectedState = { X=0;Y=0;Facing=West }
+    let roverState = navigator { turnLeft; turnLeft; turnRight }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face west after using the navigation system to turn right while facing north initially`` () =
+    let expectedState = { X=0;Y=0;Facing=West }
+    let roverState = navigator { turnLeft }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face north after using the navigation system to turn right while facing west initially`` () =
+    let expectedState = { X=0;Y=0;Facing=North }
+    let roverState = navigator { turnLeft; turnRight }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face south after using the navigation system to turn left while facing west initially`` () =
+    let expectedState = { X=0;Y=0;Facing=South }
+    let roverState = navigator { turnLeft; turnLeft }
+        
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face east after using the navigation system to turn left while facing south initially`` () =
+    let expectedState = { X=0;Y=0;Facing=East }
+    let roverState = navigator { turnLeft; turnLeft; turnLeft }
+
+    Assert.AreEqual(expectedState, roverState)
+
+[<Test>]
+let ``face north after using the navigation system to turn left while facing east initially`` () =
+    let expectedState = { X=0;Y=0;Facing=North }
+    let roverState = navigator { turnRight; turnLeft }
+
+    Assert.AreEqual(expectedState, roverState)
---
+++ MarsRover/MarsRover.fs
@@ -0,0 +1,65 @@
+﻿module MarsRover
+type CardinalDirection = North | East | South | West
+type RoverState =
+| Ready of Rover
+| Uninitialised
+and Rover = {
+    X: int
+    Y: int
+    Facing: CardinalDirection
+}
+
+let initialiseRover() = { X = 0; Y = 0; Facing = North } |> Ready
+type Direction = Right | Left
+type Command = Turn of Direction
+type Instruction = Instruction of Command*RoverState
+
+let (|FaceNorth|FaceEast|FaceSouth|FaceWest|) =
+    function
+    | Instruction ( Turn dir, Ready(rover) ) when (rover.Facing = West && dir = Right)
+            || (rover.Facing = East && dir = Left)
+            -> FaceNorth rover
+    | Instruction ( Turn dir, Ready(rover) ) when (rover.Facing = North && dir = Right)
+            || (rover.Facing = South && dir = Left)
+            -> FaceEast rover
+    | Instruction ( Turn dir, Ready(rover) ) when (rover.Facing = East && dir = Right)
+            || (rover.Facing = West  && dir = Left)
+            ->  FaceSouth rover
+    | Instruction ( Turn dir, Ready(rover) ) when (rover.Facing = North && dir = Left)
+            || (rover.Facing = South && dir = Right)
+            -> FaceWest rover
+    
+type RoverNavigatorBuilder() =
+    member __.Bind( x, f ) =
+        match x with
+        | FaceEast rover -> { rover with Facing = East }
+        | FaceSouth rover ->  { rover with Facing = South }
+        | FaceWest rover ->  { rover with Facing = West }
+        | FaceNorth rover ->  { rover with Facing = North }
+        |> Ready
+        |> f
+        
+    member __.Return ( x ) =
+        match x with
+        | Ready r -> r
+        | _ -> failwith "Rover hasn't landed yet!"
+        
+    member __.Yield _ = { X = 0; Y = 0; Facing = North }
+    member __.For _ = { X = 0; Y = 0; Facing = North }
+    member __.Zero _ = None
+    [<CustomOperation("turnRight")>]
+    member __.TurnRight rover =
+        let instruction = Instruction( Turn Right, Ready(rover) )
+        __.Bind(instruction, __.Return)
+
+    [<CustomOperation("turnLeft")>]
+    member __.TurnLeft rover =
+        let instruction = Instruction( Turn Left, Ready(rover) )
+        __.Bind(instruction, __.Return)
+
+let navigator = RoverNavigatorBuilder()
+
+let getRoverState (marsRover:RoverState) =
+    navigator {
+        return marsRover
+    }
